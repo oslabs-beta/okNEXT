e7439c4a4f6b12abcaacbd0610ada4f858c165e4
Hua Liu <hualiu@huas-air.myfiosgateway.com>
2022-11-16 16:15:41 -0500

    dont push

--- oknext/pages/_app.tsx
+++ oknext/pages/_app.tsx
@@ -1,11 +1,56 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { onCLS, onFID, onLCP } from 'web-vitals'; 
+
+// export function reportWebVitals(metric: any) {
+//   if (metric.label === 'custom' || metric.label === 'web-vital') {
+//     console.log(metric) // The metric object ({ id, name, startTime, value, label }) is logged to the console
+//   }
+// }
+
+//send metric data to database , save it
+// fetch get data from DB, display on client side
+
+//<--------------------------------------------------------------------->
+
+// export function reportWebVitals(metric: any) {
+//   switch (metric.name) {
+//     case 'FCP':
+//       console.log(metric);
+//       break;
+//     case 'LCP':
+//       console.log(metric);
+//       break;
+//     case 'CLS':
+//       console.log(metric);
+//       break;
+//     case 'FID':
+//       console.log(metric);
+//       break;
+//     case 'TTFB':
+//       console.log(metric);
+//       break;
+//     case 'INP':
+//       console.log(metric);
+//       break;
+//     case 'Next.js-hydration':
+//       console.log(metric);
+//       break;
+//     case 'Next.js-route-change-to-render':
+//       console.log(metric);
+//       break;
+//     case 'Next.js-render':
+//       console.log(metric);
+//     default:
+//       break;
+//   }
+// }
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />  
+      <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
--- oknext/pages/monitoring/monitor.tsx
+++ oknext/pages/monitoring/monitor.tsx
@@ -1,6 +1,44 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import MonitorChart from '../../components/MonitorChart';
+import { onLCP, onFID, onCLS } from 'web-vitals';
+
+onLCP(console.log);
+onFID(console.log);
+onCLS(console.log);
+
+export function reportWebVitals(metric: any) {
+  switch (metric.name) {
+    case 'FCP':
+      console.log(metric);
+      break;
+    case 'LCP':
+      console.log(metric);
+      break;
+    case 'CLS':
+      console.log(metric);
+      break;
+    case 'FID':
+      console.log(metric);
+      break;
+    case 'TTFB':
+      console.log(metric);
+      break;
+    case 'INP':
+      console.log(metric);
+      break;
+    case 'Next.js-hydration':
+      console.log(metric);
+      break;
+    case 'Next.js-route-change-to-render':
+      console.log(metric);
+      break;
+    case 'Next.js-render':
+      console.log(metric);
+    default:
+      break;
+  }
+}
 
 export default function Monitor() {
   return (
@@ -13,20 +51,19 @@ export default function Monitor() {
         <form>
           <label>
             Enter an endpoint:
-            <input 
-              type= 'text'
-              placeholder='/'
+            <input
+              type="text"
+              placeholder="/"
               //maybe url?
               //value={endpoint}
               //onChange={}
             />
           </label>
-          <button>Get Report</button>
-          {/* onClick={} */}
+          <button onClick={reportWebVitals}>Get Report</button>
         </form>
       </div>
       <div>
-        <MonitorChart/>
+        <MonitorChart />
       </div>
       <Link href="/">← Back to home</Link>
     </>
