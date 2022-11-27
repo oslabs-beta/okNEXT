1ad850905d15578fb733589a876828042faa32b8
Your Name <vivianodekhiran@gmail.com>
2022-11-26 19:13:31 -0500

    added queries for selecting data from web_vitals table

--- oknext/controllers/getDBData.js
+++ oknext/controllers/getDBData.js
@@ -15,15 +15,32 @@ const getDBData = (handler) => {
     const id = idData.rows[0]['_id']
 
     //INSERT all web_vitals, date and user id in web_vitals table 
-    query = `INSERT INTO web_vitals (date, performance, FCP, time_to_interactivity, speed_index, 
-      TBT, LCP, CLS, SEO, accessibility, best_practices, user_id, endpoint) VALUES ('${date}', ${data.performance}, ${Math.round(data.performanceScores.fcp.numericValue)}
+    query = `INSERT INTO web_vitals (date, performance, "FCP", time_to_interactivity, speed_index, 
+      "TBT", "LCP", "CLS", "SEO", accessibility, best_practices, user_id, endpoint) VALUES ('${date}', ${data.performance}, ${Math.round(data.performanceScores.fcp.numericValue)},
       ${Math.round(data.performanceScores.tti.numericValue)}, ${Math.round(data.performanceScores.speed.numericValue)}, ${Math.round(data.performanceScores.tbt.numericValue)}, 
       ${Math.round(data.performanceScores.lcp.numericValue)}, ${Math.round(data.performanceScores.cls.numericValue)}, ${data.seo}, ${data.accessibility}, 
-      ${data.bestpractices}, ${id}, ${endpoint}) RETURNING *`
+      ${data.bestpractices}, ${id}, '${endpoint}')`
+      try {
+        const userData = await db.query(query);
+        console.log(userData.rows);
+      } catch (error) {
+        console.log('opps error here', error);
+      }
+
+    //INSERT ALL next_js vitals and date into table 
+    
+
+    //SELECT for all vitals associated with the passed in user_id
+    query = `SELECT * FROM web_vitals WHERE user_id = ${id} ORDER BY _id DESC LIMIT 5`
+    try {
       const userData = await db.query(query);
-      console.log('user data here!', userData);
-      data = 'hello from DBData :)';
-    // console.log('data from DBData', data);
+      console.log(userData.rows);
+      data = userData.rows;
+    } catch(error) {
+      console.log('Error here:', error);
+    }
+      // console.log('user data here!', userData);
+      
     return handler(req, res, data);
   }
 }
