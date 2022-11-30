cf78c5f9ff90b57d7e908d9b79e2bc20a4306f73
Hua Liu <hualiu@huas-air.myfiosgateway.com>
2022-11-30 18:00:53 -0500

    turned off typescript strict mode. Run build works

--- oknext/pages/docs/UnderstandingMetrics.tsx
+++ oknext/pages/docs/UnderstandingMetrics.tsx
@@ -1,12 +1,10 @@
 import styles from '../styles/Docs.module.scss';
 import DocsNavBar from '../../components/DocsNavBar';
-import Suggestions from '../../components/Suggestions';
 
 export default function Docs() {
   return (
     <>
       <DocsNavBar />
-      <Suggestions />
 
       <div>
         {/* Audit	Weight
